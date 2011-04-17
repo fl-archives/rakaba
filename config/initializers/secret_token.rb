f10eb95e235385d47172181bfd8ec017c54c913a
anonymous785 <nomad@ag.ru>
2011-04-18 00:18:37 +0400

    initial commig

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rakaba::Application.config.secret_token = 'db396cf44ec7677826529270ab98c2fb4a0c1c7fbe9e11754d22426769f093954839c3790d59b531a18271c9edaa1eb4302cc94ecc866385397f6309ed691096'
